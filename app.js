723b826d79a33c2be9c44f84afff9f8a64f0a1af
alka222 <alkabharadwaj222@gmail.com>
2023-07-23 22:19:02 +0530

    adding and displaying cart items

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
   User.findById('64b830403049afc6e710b841')
     .then(user => {
-      req.user = user;
+       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
