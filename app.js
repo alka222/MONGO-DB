037f23e9e5803b0d7351d2a1a2d73c99952d5663
alka222 <alkabharadwaj222@gmail.com>
2023-07-26 16:31:40 +0530

    cart system adding and displaying items

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('')
+  User.findById('64c0ecab9f635c8dca6a9424')
     .then(user => {
       req.user = user;
       next();
